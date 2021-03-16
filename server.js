018986f66509a546fb442ca3ae3b22ad46a10a91
sureshch97 <sureshmudiraj563@gmail.com>
2021-03-16 10:09:16 +0530

    Prepared of deploy

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
-const connectDB = require('./config/db')
-const path = require('path')
+const connectDB = require('./config/db');
+const path = require('path');
+
+
+const app = express();
 
 
 //connect to db
